41ab588df17fac63bd10c4c8305f9ac63e0d5c29
Nazreen <nazreen@gmail.com>
2026-04-30 15:04:37 +0530

    middlewear, tokens

---
+++ admin/src/pages/Add.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Add = () => {
+  return (
+    <form>
+        <div>
+            <p>Upload Image</p>
+            <div>
+                <label htmlFor="image1">
+                    <img src={assets.upload_area} alt="" />
+                    <input type="file" id="image1" hidden/>
+                    <img src={assets.upload_area} alt="" />
+                    <input type="file" id="image1" hidden/>
+
+                    <img src={assets.upload_area} alt="" />
+                    <input type="file" id="image1" hidden/>
+
+                </label>
+            </div>
+        </div>
+    </form>
+  )
+}
+
+export default Add
